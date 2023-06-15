f3665df127600b00bfa5d9f8fadbeabc842d7a76
Vincent Spiewak <vspiewak@gmail.com>
2023-06-15 22:39:15 +0200

    Use generics for binarysearch

--- search/binary.go
+++ search/binary.go
@@ -1,16 +1,17 @@
 package search
 
-func BinaryRec(values []int, key int) (int, error) {
-	return binary_recursive(values, key, 0, len(values)-1)
+import . "github.com/vspiewak/go-algorithms/constraints"
+
+func BinaryRec[T Ordered](values []T, key T) (int, error) {
+	return binary_recursive[T](values, key, 0, len(values)-1)
 }
 
-func binary_recursive(values []int, key int, start int, end int) (int, error) {
+func binary_recursive[T Ordered](values []T, key T, start int, end int) (int, error) {
 
 	if start > end {
 		return -1, ErrNotFound
 	}
 
-	//mid := (start + end) / 2
 	mid := start + ((end - start) / 2)
 
 	if values[mid] == key {
@@ -23,7 +24,7 @@ func binary_recursive(values []int, key int, start int, end int) (int, error) {
 
 }
 
-func BinaryIter(values []int, key int) (int, error) {
+func BinaryIter[T Ordered](values []T, key T) (int, error) {
 
 	start := 0
 	end := len(values) - 1
@@ -42,23 +43,3 @@ func BinaryIter(values []int, key int) (int, error) {
 	return -1, ErrNotFound
 
 }
-
-/*
-func BinaryGen[K comparable](values []K, key K) (int, error) {
-	start := 0
-	end := len(values) - 1
-
-	for start <= end {
-		mid := start + ((end - start) / 2)
-		if values[mid] == key {
-			return mid, nil
-		} else if key < values[mid] {
-			end = mid - 1
-		} else {
-			start = mid + 1
-		}
-	}
-
-	return -1, ErrNotFound
-}
-*/
